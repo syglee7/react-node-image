761fef50457fe48b40060679eecbef6498401dc6
zenaislee <syglee7@gmail.com>
2023-06-01 04:33:44 +0900

    add dotenv

--- server/server.js
+++ server/server.js
@@ -1,33 +1,45 @@
 const express = require("express");
-const multer = require('multer')
-const { v4: uuid } = require('uuid')
-const mime = require("mime-types")
+const multer = require("multer");
+const { v4: uuid } = require("uuid");
+const mime = require("mime-types");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "./uploads"),
-    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
-})
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) =>
+    cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+});
 
-const upload = multer({ storage, fileFilter: (req,file,cb) => {
-        if (["image/png", "image/jpeg"].includes(file.mimetype)) {
-            cb(null, true);
-        } else {
-            cb(new Error('invalid file type'), false)
-        }
-
-    },
-    limits: {
-        fileSize: 1024 * 1024 * 3
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (["image/png", "image/jpeg"].includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("invalid file type"), false);
     }
-})
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 3,
+  },
+});
 const app = express();
 const PORT = 5001;
 
-app.use("/uploads", express.static("uploads"))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("mongodb connect");
+    app.use("/uploads", express.static("uploads"));
 
-app.post('/upload', upload.single("image"),(req, res) => {
-    console.log(req.file);
-    res.json(req.file)
-});
+    app.post("/upload", upload.single("image"), (req, res) => {
+      console.log(req.file);
+      res.json(req.file);
+    });
 
-app.listen(PORT, () => console.log(`Express server listening on PORT ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Express server listening on PORT ${PORT}`)
+    );
+  })
+  .catch(console.err);
