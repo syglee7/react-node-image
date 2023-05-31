1d57a4098d32b3f850f09c48ff24e10a8575706f
zenaislee <syglee7@gmail.com>
2023-06-01 04:11:05 +0900

    first commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const multer = require('multer')
+const { v4: uuid } = require('uuid')
+const mime = require("mime-types")
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, "./uploads"),
+    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
+})
+
+const upload = multer({ storage, fileFilter: (req,file,cb) => {
+        if (["image/png", "image/jpeg"].includes(file.mimetype)) {
+            cb(null, true);
+        } else {
+            cb(new Error('invalid file type'), false)
+        }
+
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 3
+    }
+})
+const app = express();
+const PORT = 5001;
+
+app.use("/uploads", express.static("uploads"))
+
+app.post('/upload', upload.single("image"),(req, res) => {
+    console.log(req.file);
+    res.json(req.file)
+});
+
+app.listen(PORT, () => console.log(`Express server listening on PORT ${PORT}`))
